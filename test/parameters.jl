4e4ece380e989ae12226ed24a51f425794249cfc
Benoit Pasquier <pasquieb@uci.edu>
2019-05-13 10:10:25 +1000

    fix Parameters Int -> Float

--- test/parameters.jl
+++ test/parameters.jl
@@ -4,7 +4,7 @@ add_parameter!(t, :DIPgeo, 2.3u"mmol/m^3",
     optimizable = true,
     description = "Mean DIP concentration",
     LaTeX = "\\xNUT^\\obs")
-add_parameter!(t, :τg, 1u"Myr",
+add_parameter!(t, :τg, 1.0u"Myr",
     description = "Geological restoring timescale",
     LaTeX = "\\tau_\\mathrm{geo}")
 add_parameter!(t, :Umax, 1e-3u"mmol/m^3/d",
@@ -15,14 +15,14 @@ add_parameter!(t, :ku, 1e-1u"mmol/m^3",
     optimizable = true,
     description = "Half-saturation constant (Michaelis-Menten)",
     LaTeX = "k_\\vec{u}")
-add_parameter!(t, :z₀, 80u"m",
+add_parameter!(t, :z₀, 80.0u"m",
     description = "Depth of the euphotic layer base",
     LaTeX = "z_0")
-add_parameter!(t, :w₀, 1u"m/d",
+add_parameter!(t, :w₀, 1.0u"m/d",
     optimizable = true,
     description = "Sinking velocity at surface",
     LaTeX = "w_0")
-add_parameter!(t, :w′, 1u"d^-1",
+add_parameter!(t, :w′, 1.0u"d^-1",
     optimizable = true,
     description = "Vertical gradient of sinking velocity",
     LaTeX = "w'")
