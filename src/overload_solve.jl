232009e0fb2c678c40fd3709db75ac5b6fd4fc13
Benoit Pasquier <pasquieb@uci.edu>
2019-07-02 18:32:37 +1000

    Removed unused parts

--- src/overload_solve.jl
+++ src/overload_solve.jl
@@ -1,12 +1,21 @@
 using DiffEqBase
 
 struct CTKAlg <: DiffEqBase.AbstractSteadyStateAlgorithm end
-struct CTKAlg2 <: DiffEqBase.AbstractSteadyStateAlgorithm end
 
+"""
+    solve(prob::DiffEqBase.AbstractSteadyStateProblem,
+          alg::CTKAlg;
+          nrm=norm,
+          τstop=1e12*365*24*60*60,
+          preprint="",
+          maxItNewton=50)
+
+Solves `prob` using the modified C.T.Kelley Shamanskii algorithm.
+"""
 function DiffEqBase.solve(prob::DiffEqBase.AbstractSteadyStateProblem,
                           alg::CTKAlg;
                           nrm=norm,
-                          τstop=1e12*365*24*60*60,
+                          τstop=ustrip(1e6"Myr"|>u"s"),
                           preprint="",
                           maxItNewton=50)
     # Define the functions according to DiffEqBase.SteadyStateProblem type
@@ -18,26 +27,6 @@ function DiffEqBase.solve(prob::DiffEqBase.AbstractSteadyStateProblem,
     ∇ₓF(x) = prob.f(df, dx, x, p, t)
     # Compute `u_steady` and `resid` as per DiffEqBase using my algorithm
     x_steady = NewtonChordShamanskii(F, ∇ₓF, nrm, x0, τstop; preprint=preprint, maxItNewton=maxItNewton)
-    resid = F(x_steady)
-    # Return the common DiffEqBase solution type
-    DiffEqBase.build_solution(prob, alg, x_steady, resid; retcode=:Success)
-end
-
-function DiffEqBase.solve(prob::DiffEqBase.AbstractSteadyStateProblem,
-                          alg::CTKAlg2;
-                          nrm=norm,
-                          τstop=1e12*365*24*60*60,
-                          preprint="",
-                          maxItNewton=50)
-    # Define the functions according to DiffEqBase.SteadyStateProblem type
-    p = prob.p
-    t = 0
-    x0 = copy(prob.u0)
-    dx, df = copy(x0), copy(x0)
-    F(x) = prob.f(dx, x, p, t)
-    ∇ₓF(x) = prob.f(df, dx, x, p, t)
-    # Compute `u_steady` and `resid` as per DiffEqBase using my algorithm
-    x_steady = NewtonChordShamanskii2(F, ∇ₓF, nrm, x0, τstop; preprint=preprint, maxItNewton=maxItNewton)
     resid = F(x_steady)
     # Return the common DiffEqBase solution type
     DiffEqBase.build_solution(prob, alg, x_steady, resid; retcode=:Success)
