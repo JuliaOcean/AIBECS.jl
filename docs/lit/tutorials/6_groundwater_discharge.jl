5d30fb3702561c2f4ea5267de086aa1f001a8117
Benoit Pasquier <briochemc@gmail.com>
2020-08-21 21:22:05 +1000

    Revert remove clim for png fmt

--- docs/lit/tutorials/6_groundwater_discharge.jl
+++ docs/lit/tutorials/6_groundwater_discharge.jl
@@ -56,7 +56,7 @@ using Plots
 scatter([x.lon for x in gws], [x.lat for x in gws],
         zcolor=log10.(ustrip.([x.VFR for x in gws] / u"m^3/s")),
         colorbartitle="log₁₀(discharge / (1 m³ s⁻¹))",
-        fmt=:png)
+        clim=(0,10), fmt=:png)
 
 # We can regrid these into the OCIM2 grid and return the corresponding vector with
 
