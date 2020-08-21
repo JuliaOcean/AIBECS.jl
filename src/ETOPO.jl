50e2f0caecbedb0ba4544ddbffbe4a25e90a0d98
Benoit Pasquier <briochemc@gmail.com>
2020-08-21 12:08:51 +1000

    Add roughness from ETOPO (WIP)

--- src/ETOPO.jl
+++ src/ETOPO.jl
@@ -5,6 +5,7 @@ using Dates
 using Unitful
 using DataDeps              # For storage location of data
 using NCDatasets
+using Distances
 
 function fallback_download(remotepath, localdir)
     @assert(isdir(localdir))
@@ -104,6 +105,40 @@ end
 fractiontopo(bin3D, grd) = vectorize(bin3D ./ repeat(sum(bin3D, dims=3), outer=(1, 1, size(grd)[3])), grd)
 export fractiontopo
 
+function roughnesstopo(grd)
+    @warn "Estimating roughness from ETOPO. This may take a few seconds"
+    Z, lats, lons = load()
+    R_earth = ustrip(u"m", 6371.0u"km")
+    δlon = step(lons)
+    δlat = step(lons)
+    midlats = 0.5(lats[1:end-1] + lats[2:end])
+    midlons = 0.5(lons[1:end-1] + lons[2:end])
+    δxs = [Haversine(R_earth)((0.0, lat), (δlon, lat)) for lat in midlats]
+    δy = Haversine(R_earth)((lons[1], 0.0), (lons[1], δlat))
+    A_lon = abs.(diff(Z, dims=1)) .* δy
+    Z_lon = 0.5(Z[1:end-1,:] + Z[2:end,:])
+    A_lat = abs.(diff(Z, dims=2)) .* repeat(reshape(δxs, (1, length(δxs))), outer=(size(Z,1), 1))
+    Z_lat = 0.5(Z[:,1:end-1] + Z[:,2:end])
+    return sumroughness(A_lon, Z_lon, lats, midlons, grd) + sumroughness(A_lat, Z_lat, midlats, lons, grd)
+end
+
+function sumroughness(A::AbstractArray{T,2}, Z, lats, lons, grd) where T
+    roughness = zeros(size(grd))
+    edepth = ustrip.(grd.depth .+ 0.5grd.δdepth)
+    elon = ustrip.(grd.lon .+ 0.5grd.δlon)
+    elat = ustrip.(grd.lat .+ 0.5grd.δlat)
+    kmax = dropdims(count(iswet(grd), dims=3), dims=3)
+    for (j,lat) in enumerate(lats)
+        jgrd = searchsortedfirst(elat, lat)
+        for (i,lon) in enumerate(lons)
+            igrd = searchsortedfirst(elon, mod(lon, 360))
+            kmax[jgrd,igrd] == 0 && continue
+            kgrd = searchsortedfirst(view(edepth, 1:kmax[jgrd, igrd]-1), -Z[i,j])
+            roughness[jgrd, igrd, kgrd] += A[i,j]
+        end
+    end
+    return roughness[iswet(grd)]
+end
 
 end # module
 
