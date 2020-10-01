83303e72b96def2f5c34ba9707fc86566c50caed
Benoit Pasquier <briochemc@gmail.com>
2020-10-01 15:24:03 +1000

    Add tests for partial burial/remin of particles

--- test/particles.jl
+++ test/particles.jl
@@ -1,10 +1,23 @@
 @testset "Particles" begin
-    T = transportoperator(grd, 100.0)
-    @test T isa SparseMatrixCSC
-    @testset "vᵀ T = 0 (T conserves mass)" begin
+    @testset "scalar settling velocity" begin
+        T = transportoperator(grd, 100.0)
+        @test T isa SparseMatrixCSC
         @test norm(v) / norm(T' * v) > ustrip(upreferred(1u"Myr"))
     end
 
+    @testset "settling velocity as a function" begin
+        w(z) = upreferred(100u"m/d" + z * u"m" / 100u"m" * 10u"m/d")
+        @testset "no sed remin" begin
+            T = transportoperator(grd, w)
+            @test T isa SparseMatrixCSC
+            @test norm(v) / norm(T' * v) > ustrip(upreferred(1u"Myr"))
+        end
 
+        @testset "fractional sed remin" begin
+            T = transportoperator(grd, w, fsedremin=0.5)
+            @test T isa SparseMatrixCSC
+            @test norm(v) / norm(T' * v) < ustrip(upreferred(1u"yr"))
+        end
+    end
 
 end
