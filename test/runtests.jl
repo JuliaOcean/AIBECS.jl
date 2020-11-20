4d01109369c85f4a4630c0a86487c9fa678efa9c
Benoit Pasquier <briochemc@gmail.com>
2020-11-20 23:57:31 +1100

    Readd all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,44 +14,27 @@ using Plots
 # For CI, make sure the downloads do not hang
 ENV["DATADEPS_ALWAYS_ACCEPT"] = true
 
-##test_setup_only = [:OCIM1, :OCIM0, :OCCA]
-### Using `include` evaluates at global scope,
-### so `Circulation` must be changed at the global scope too.
-### This is why there is an `eval` in the for loop(s) below
-##@testset "test setup.jl only" for C in test_setup_only
-##    @testset "$C" begin
-##        eval(:(Circulation = $C))
-##        include("setup.jl")
-##    end
-##end
-##
-##
-##test_plots = [:OCIM2]
-##@testset "Test setup, plots, and sources" for C in test_plots
-##    @testset "$C" begin
-##        eval(:(Circulation = $C))
-##        include("setup.jl")
-##        include("plots.jl")
-##        include("sources.jl")
-##    end
-##end
-##
-##
-##include("parameters.jl")
-##test_everything = [:Primeau_2x2x2, :TwoBoxModel, :Archer_etal_2000]
-##@testset "test everything" for C in test_everything
-##    @testset "$C" begin
-##        eval(:(Circulation = $C))
-##        include("setup.jl")
-##        include("particles.jl")
-##        include("bgc_functions.jl")
-##        include("gridtools.jl")
-##        include("cost_functions.jl")
-##        include("solvers.jl")
-##        include("time_steps.jl")
-##        include("derivatives.jl")
-##    end
-##end
+test_setup_only = [:OCIM1, :OCIM0, :OCCA]
+# Using `include` evaluates at global scope,
+# so `Circulation` must be changed at the global scope too.
+# This is why there is an `eval` in the for loop(s) below
+@testset "test setup.jl only" for C in test_setup_only
+    @testset "$C" begin
+        eval(:(Circulation = $C))
+        include("setup.jl")
+    end
+end
+
+
+test_plots = [:OCIM2]
+@testset "Test setup, plots, and sources" for C in test_plots
+    @testset "$C" begin
+        eval(:(Circulation = $C))
+        include("setup.jl")
+        include("plots.jl")
+        include("sources.jl")
+    end
+end
 
 
 include("parameters.jl")
