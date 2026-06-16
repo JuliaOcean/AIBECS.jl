c31ad956a60849ed8dcf6e3c15fb730a0b89fd03
gaelforget <gforget@mit.edu>
2026-06-15 21:57:05 -0400

    add two panel examples (new plot!)

--- ext/AIBECSMakieExt.jl
+++ ext/AIBECSMakieExt.jl
@@ -3,7 +3,7 @@ module AIBECSMakieExt
 import AIBECS, Makie
 import AIBECS.demo: SteadyStateSolution, @unpack
 import Unitful: ustrip, upreferred
-import Makie: plot
+import Makie: plot, plot!
 
 function get_axis(grd)
     lon = ustrip.(grd.lon)
@@ -27,30 +27,58 @@ plot(results,depth=2000)
 function plot(results::SteadyStateSolution; 
     lon = nothing, lat = nothing, depth = nothing, kwargs...)
 
+    fig = Makie.Figure()
+    axi = Makie.Axis(fig[1, 1])
+    hm=plot!(results; lon = lon, lat = lat, depth = depth, kwargs...)
+    Makie.Colorbar(fig[1, 2], hm)
+    fig
+end
+
+"""
+    plot!(results::SteadyStateSolution; 
+        lon = nothing, lat = nothing, depth = nothing, kwargs...)
+
+```
+using AIBECS, GLMakie, JLD2
+results=AIBECS.demo.demo1()
+
+begin
+    fig = Makie.Figure();
+    Makie.Axis(fig[1, 1]);
+    plot!(results,depth=1000,colorrange=(0,5e10))
+    Makie.Axis(fig[1, 2]);
+    plot!(results,depth=2000,colorrange=(0,5e10))
+    fig
+end
+```
+"""
+function plot!(results::SteadyStateSolution; 
+    lon = nothing, lat = nothing, depth = nothing, kwargs...)
+
     @unpack grd, state = results
     ax=get_axis(grd)
+    axi=Makie.current_axis()
 
-    fig = Makie.Figure()
     if !isnothing(depth)
         slice=AIBECS.horizontalslice(state, grd; depth = depth);
-        axi = Makie.Axis(fig[1, 1],title = "state at $(depth) m",
-            xticks = ax.lonticks,  yticks = ax.latticks)
-        hm=Makie.heatmap!(axi, ax.lon, ax.lat, slice')
+        axi.title = "state at $(depth) m"
+        axi.xticks = ax.lonticks
+        axi.yticks = ax.latticks
+        hm=Makie.heatmap!(axi, ax.lon, ax.lat, slice';kwargs...)
     elseif !isnothing(lon)
         mer = AIBECS.meridionalslice(state, grd; lon = lon)
         axi = Makie.Axis(fig[1, 1],title = "Meridional slice at $lon",
             ylabel = "Depth (m)", xticks = ax.latticks, yreversed = true)
-        hm=Makie.heatmap!(axi, ax.lat, ax.depth, mer')
+        hm=Makie.heatmap!(axi, ax.lat, ax.depth, mer',kwargs...)
     elseif !isnothing(lat)
         mer = AIBECS.zonalslice(state, grd; lat = lat)
         axi = Makie.Axis(fig[1, 1],title = "Zonal slice at $lat",
             ylabel = "Depth (m)", xticks = ax.lonticks, yreversed = true)
-        hm=Makie.heatmap!(axi, ax.lon, ax.depth, mer')
+        hm=Makie.heatmap!(axi, ax.lon, ax.depth, mer',kwargs...)
     else
         error("unknown option")
     end
-    Makie.Colorbar(fig[1, 2], hm)
-    fig
+    hm
 end
 
 end
